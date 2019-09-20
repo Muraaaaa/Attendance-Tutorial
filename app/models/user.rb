db61bf50d386f91b82b45c45c7a7a59cfdbf3e90
mura <iesu.madao@gmail.com>
2019-09-20 09:27:25 +0000

    Finish editing-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true    
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   def User.digest(string)
     cost = 
@@ -30,8 +30,9 @@ class User < ApplicationRecord
   end
    
   def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # ダイジェストが存在しない場合はfalseを返して終了します。
+   return false if remember_digest.nil?
+   BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
   def forget
